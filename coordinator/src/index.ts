0119ccd6663e30f600457274aa76b5921bd8854a
ahmed <ahmedsaheed2@outlook.com>
2024-05-05 15:23:09 +0100

    feat(core): dockerfiles - first pass

--- coordinator/src/index.ts
+++ coordinator/src/index.ts
@@ -1,42 +1,27 @@
-import express from 'express'
-import spawn from 'child_process'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+const app = express();
+const PORT = process.env.PORT || 8080;
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-    console.log(req.body)
-    res.json({ message: 'Hello World' })
-})
+app.post("/compile", (req, res) => {
+  const { code, lang } = req.body;
+  if (!code || !lang) {
+    console.log("Code and lang are required fields");
+    return res
+      .status(400)
+      .json({ message: "Code and lang are required fields" });
+  }
+  const isCompilable = lang === "java" || lang === "c" || lang === "python";
+  let imageName = isCompilable ? `${lang}-compiler` : undefined;
+  console.log(lang, code, imageName);
+  imageName != undefined
+    ? res.json({ lang, code, imageName })
+    : res.status(400).json({ message: "Invalid language" });
+});
 
-app.post('/compile', (req, res) => {
-    const { code, lang } = req.body
-    if (!code || !lang) {
-        console.log('Code and lang are required fields')
-        return res
-            .status(400)
-            .json({ message: 'Code and lang are required fields' })
-    }
-    let imageName
-    switch (lang) {
-        case 'python':
-            imageName = 'python3_compiler_service'
-            break
-        case 'java':
-            imageName = 'java_compiler_service'
-            break
-        case 'c':
-            imageName = 'c_compiler_service'
-            break
-        default:
-            return res.status(400).json({ message: 'Invalid language' })
-    }
-    console.log(lang, code)
-    res.json({ lang, code })
-})
-
-app.listen(8080, () => {
-    console.log('Server is running on port 8080')
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
